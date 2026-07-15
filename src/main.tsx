1988ae4b2d50a9f31ded1557d2b40d54110ba982
Bernard Krehula <krehula.bernard456@gmail.com>
2026-07-15 22:03:15 +0200

    refactor: nest queryClient and reactRouter inside cssBaseLine and themeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,19 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 import router from "./routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "#/theme/theme";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </ThemeProvider>
   </StrictMode>,
 );
