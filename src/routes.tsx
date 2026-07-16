0070116754b171d637f233a9a5552434506a61b9
Bernard Krehula <krehula.bernard456@gmail.com>
2026-07-16 20:20:49 +0200

    refactor: change path poslovi to <Jobs /> component

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "#/layouts/MainLayout";
 import Dashboard from "#/pages/dashboard";
+import Jobs from "#/pages/jobs";
 import ComingSoon from "#/pages/ComingSoon";
 
 const router = createBrowserRouter([
@@ -9,7 +10,7 @@ const router = createBrowserRouter([
     element: <MainLayout />,
     children: [
       { index: true, element: <Dashboard /> },
-      { path: "poslovi", element: <ComingSoon title="Poslovi" /> },
+      { path: "poslovi", element: <Jobs /> },
       { path: "cjenik", element: <ComingSoon title="Cjenik" /> },
       { path: "postavke", element: <ComingSoon title="Postavke" /> },
     ],
