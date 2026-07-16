b3dc163e3ad21318c02c7b796a3201091eb31592
Bernard Krehula <krehula.bernard456@gmail.com>
2026-07-16 20:19:09 +0200

    feat: add type JobType

---
+++ src/types/Job.type.ts
@@ -0,0 +1,11 @@
+export type JobType = {
+  id: string;
+  address: string;
+  client_name: string;
+  phone: string;
+  date: string;
+  notes: string | null;
+  created_at: string;
+  date_started: string;
+  date_finished: string | null;
+};
