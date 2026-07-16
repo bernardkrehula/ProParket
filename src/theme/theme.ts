5a5c5c9ced653a87816c0191359a8eeb0afdc7de
Bernard Krehula <krehula.bernard456@gmail.com>
2026-07-16 20:21:32 +0200

    style: add style on MuiButton

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -34,6 +34,13 @@ const theme = createTheme({
         },
       },
     },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "none",
+        },
+      },
+    },
   },
 });
 
