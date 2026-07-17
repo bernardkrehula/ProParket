b9b4ab285265c039d3e3aae077bb237dbeeac869
Bernard Krehula <krehula.bernard456@gmail.com>
2026-07-17 21:47:18 +0200

    refactor: add new date formaters to existing format file

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -7,3 +7,14 @@ const currencyFormatter = new Intl.NumberFormat("hr-HR", {
 export const formatCurrency = (value: number) => {
   return currencyFormatter.format(value);
 };
+
+const dateFormatter = new Intl.DateTimeFormat("hr-HR", {
+  day: "2-digit",
+  month: "2-digit",
+  year: "numeric",
+});
+
+export const formatDate = (value: string | null) => {
+  if (!value) return "-";
+  return dateFormatter.format(new Date(value));
+};
