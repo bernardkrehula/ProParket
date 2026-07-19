aff452f4652a1091923460b1ebfd76592c92f686
Bernard Krehula <krehula.bernard456@gmail.com>
2026-07-19 15:53:53 +0200

    refactor: change router path

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
-import router from "./routes";
+import router from "./routes/router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
