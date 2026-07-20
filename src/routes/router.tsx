2b56b61315adb1baa0ea5af7cadb8e80da2fd6ae
Bernard Krehula <krehula.bernard456@gmail.com>
2026-07-20 22:34:15 +0200

    refactor: add comma after commented code

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
         path: "/",
         element: <MainLayout />,
         children: [
-          {/* { index: true, element: <Dashboard /> }, */}
+          {/* { index: true, element: <Dashboard /> }, */},
           { index: true, element: <Jobs /> },
           { path: "poslovi", element: <Jobs /> },
           { path: "raspored", element: <Schedule /> },
