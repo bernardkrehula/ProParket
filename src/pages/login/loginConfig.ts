5aa64bdbb37ca25def9c4647bec6e620077599c2
Bernard Krehula <krehula.bernard456@gmail.com>
2026-07-21 21:37:48 +0200

    refactor(style): change style on loginConfig file

--- src/pages/login/loginConfig.ts
+++ src/pages/login/loginConfig.ts
@@ -1,5 +1,12 @@
 import type { SxProps, Theme } from "@mui/material";
 
+export const loginLoadingSx: SxProps<Theme> = {
+  display: "flex",
+  justifyContent: "center",
+  py: 10,
+  marginTop: 25,
+};
+
 export const loginRootSx: SxProps<Theme> = {
   display: "flex",
   alignItems: "center",
@@ -13,6 +20,7 @@ export const loginCardSx: SxProps<Theme> = {
   width: "100%",
   maxWidth: 400,
   p: 4,
+  pb: 7,
   borderColor: "divider",
   display: "flex",
   flexDirection: "column",
