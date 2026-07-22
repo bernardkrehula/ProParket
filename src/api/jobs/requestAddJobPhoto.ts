1c3edd30a3ded7c0d8fdf770d1ebc191f0d62da6
Bernard Krehula <krehula.bernard456@gmail.com>
2026-07-22 18:42:05 +0200

    fix: imageCompression add in requestAddJobPhoto async fucntion

--- src/api/jobs/requestAddJobPhoto.ts
+++ src/api/jobs/requestAddJobPhoto.ts
@@ -1,21 +1,28 @@
 import supabase from "#/config/supabaseClientVite";
 import { GenericError } from "#/utils/GenericError";
 import { handleSupabaseError } from "#/lib/handleSupabaseError";
+import imageCompression from "browser-image-compression";
 
 export const MAX_JOB_PHOTO_SIZE_BYTES = 1 * 1024 * 1024;
 
 export const requestAddJobPhoto = async (jobId: string, photo: File) => {
-  if (photo.size > MAX_JOB_PHOTO_SIZE_BYTES) {
+  const compressedPhoto = await imageCompression(photo, {
+    maxSizeMB: 1,
+    maxWidthOrHeight: 1024,
+    useWebWorker: true,
+  });
+
+  if (compressedPhoto.size > MAX_JOB_PHOTO_SIZE_BYTES) {
     throw new GenericError();
   }
 
-  const filePath = `${jobId}/${Date.now()}-${photo.name}`;
+  const filePath = `${jobId}/${Date.now()}-${compressedPhoto.name}`;
 
   const response = await supabase.storage
     .from("job-photos")
-    .upload(filePath, photo);
-
-    const authError = handleSupabaseError(response);
+    .upload(filePath, compressedPhoto);
+  console.log(compressedPhoto)
+  const authError = handleSupabaseError(response);
   if (authError) return authError;
 
   return response;
