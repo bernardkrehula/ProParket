ad1866f6815a3b35c5d98fb7ce4ab32b7e8d403e
Bernard Krehula <krehula.bernard456@gmail.com>
2026-07-22 18:42:55 +0200

    refactor: response.error pass to isAuthApiError

--- src/lib/handleSupabaseError.ts
+++ src/lib/handleSupabaseError.ts
@@ -3,6 +3,6 @@ import { isAuthApiError } from "@supabase/supabase-js";
 
 export const handleSupabaseError = <T>(response: { error: T | null }) => {
   if (!response.error) return null;
-  if (isAuthApiError(response)) return response.error;
+  if (isAuthApiError(response.error)) return response.error;
   throw new GenericError();
 };
